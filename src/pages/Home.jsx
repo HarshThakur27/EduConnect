44068f62e1c84102ff5a1247b38f1d01f0adfdf8
Harsh Thakur <121555585+HarshThakur27@users.noreply.github.com>
2024-08-29 11:34:22 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,22 +15,22 @@ import ExploreMore from "../components/core/HomePage/ExploreMore"
 // import reviews from "../Data1";
 // import Testimonal from './Testimonal'
 const Home = () => {
-    useEffect(() => {
-        const script1 = document.createElement("script");
-        script1.src = "https://cdn.botpress.cloud/webchat/v2.1/inject.js";
-        script1.async = true;
-        document.body.appendChild(script1);
+    // useEffect(() => {
+    //     const script1 = document.createElement("script");
+    //     script1.src = "https://cdn.botpress.cloud/webchat/v2.1/inject.js";
+    //     script1.async = true;
+    //     document.body.appendChild(script1);
 
-        const script2 = document.createElement("script");
-        script2.src = "https://mediafiles.botpress.cloud/d7784fab-f8e2-4c14-ae8f-1050bb16f99e/webchat/v2.1/config.js";
-        script2.async = true;
-        document.body.appendChild(script2);
+    //     const script2 = document.createElement("script");
+    //     script2.src = "https://mediafiles.botpress.cloud/d7784fab-f8e2-4c14-ae8f-1050bb16f99e/webchat/v2.1/config.js";
+    //     script2.async = true;
+    //     document.body.appendChild(script2);
 
-        return () => {
-            document.body.removeChild(script1);
-            document.body.removeChild(script2);
-        };
-    }, []);
+    //     return () => {
+    //         document.body.removeChild(script1);
+    //         document.body.removeChild(script2);
+    //     };
+    // }, []);
   return (
     <div>
       {/*Section1  */}
