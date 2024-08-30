b1eec14602643fac4325376c0ea8d2fcb7d8e409
Harsh Thakur <121555585+HarshThakur27@users.noreply.github.com>
2024-08-30 11:16:30 +0530

    Update mailSender.js

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -12,7 +12,7 @@ const mailSender = async (email, title, body) => {
 
 
             let info = await transporter.sendMail({
-                from: 'StudyNotion || CodeHelp - by Babbar',
+                from: 'EduConnect || by Harsh Thakur',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
